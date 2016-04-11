86f25f9c16f164f616a94ecf7a72cef66fdb8ed6
Johan Lindell <johan@lindell.me>
2016-04-11 12:58:49 +0200

    Refacoring of code from the forked repo. Increased handlebars version

--- index.js
+++ index.js
@@ -12,21 +12,6 @@ function render(resume) {
 	// Get a country from the country code
 	resume.basics.location.country = countries[resume.basics.location.countryCode];
 
-  // http://stackoverflow.com/a/12002281/1263876
-	// TODO
-  Handlebars.registerHelper("foreach",function(arr,options) {
-      if(options.inverse && !arr.length)
-          return options.inverse(this);
-
-      return arr.map(function(item,index) {
-          item.$index = index;
-          item.$first = index === 0;
-          item.$notfirst = index !== 0;
-          item.$last  = index === arr.length-1;
-          return options.fn(item);
-      }).join('');
-  });
-
 	// Remove http and www from url
 	Handlebars.registerHelper('no-http', function(options) {
 		this.url = this.url.replace(/(https?:\/\/)?(www\.)?/, "");
@@ -38,36 +23,22 @@ function render(resume) {
 		return "fa-" + str.toLowerCase() + "-square";
 	});
 
-  // http://stackoverflow.com/a/16315366
-  Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
-    switch (operator) {
-        case '==':
-            return (v1 == v2) ? options.fn(this) : options.inverse(this);
-        case '===':
-            return (v1 === v2) ? options.fn(this) : options.inverse(this);
-        case '<':
-            return (v1 < v2) ? options.fn(this) : options.inverse(this);
-        case '<=':
-            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
-        case '>':
-            return (v1 > v2) ? options.fn(this) : options.inverse(this);
-        case '>=':
-            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
-        case '&&':
-            return (v1 && v2) ? options.fn(this) : options.inverse(this);
-        case '||':
-            return (v1 || v2) ? options.fn(this) : options.inverse(this);
-        default:
-            return options.inverse(this);
-    }
-  });
+  // http://stackoverflow.com/a/31632215/838789
+	Handlebars.registerHelper({
+	    and: function (v1, v2) {
+	        return v1 && v2;
+	    },
+	    or: function (v1, v2) {
+	        return v1 || v2;
+	    }
+	});
 
   // http://stackoverflow.com/a/18831911
   Handlebars.registerHelper('commalist', function(items, options) {
     return options.fn(items.join(', '));
   });
 
-  //Uncomment this for printing as .pdf
+  // Send all necessary resources to the handlebars template and compile it
 	return Handlebars.compile(template)({
 		resume: resume,
 		standardCSS: standardCSS,
